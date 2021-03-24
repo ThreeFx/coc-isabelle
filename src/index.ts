00702ce2d5160510fd77ae46842839d99ef66d71
Ben Fiedler <git@bfiedler.ch>
2021-03-24 21:23:51 +0100

    Provide isabelle.openOutput command

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-import {CodeActionProvider, ExtensionContext, LanguageClient, LanguageClientOptions, languages, ProviderResult, ServerOptions, services, window, workspace} from 'coc.nvim';
+import {commands, CodeActionProvider, ExtensionContext, LanguageClient, LanguageClientOptions, languages, ProviderResult, ServerOptions, services, window, workspace} from 'coc.nvim';
 import {CancellationToken, CodeAction, CodeActionContext, CodeActionKind, Command, Range} from 'vscode-languageserver-protocol';
 import {TextDocument} from 'vscode-languageserver-textdocument';
 
 export async function activate(context: ExtensionContext): Promise<void> {
-    const config = workspace.getConfiguration('coc-isabelle')
+    const config = workspace.getConfiguration('isabelle')
     const isEnabled = config.get<boolean>('enable', true)
     if (!isEnabled) {
         return
     }
-
-    let {logger} = context;
-    logger.info(`extension from ${context.extensionPath}`)
 
     const serverOptions: ServerOptions = {
         command: 'isabelle',
@@ -47,25 +44,21 @@ export async function activate(context: ExtensionContext): Promise<void> {
             _context: CodeActionContext,
             _token: CancellationToken
         ): ProviderResult<(Command | CodeAction)[]> => {
-            client.error('code action provider called')
+            client.info('code action provider called')
             if (range.start.line != range.end.line) {
                 return null
             }
-            client.error('is on same line')
             const linenr = range.start.line
             return new Promise<(Command | CodeAction)[]>(resolve => {
                 workspace.nvim.line.then(line => {
                     for (const method of ['try0', 'try', 'sledgehammer']) {
                         const startcol = line.indexOf(method)
                         if (startcol != -1) {
-                            client.error('has method: ' + method)
                             isaBuffer.getLines({start: 0, end: -1}).then((lines) => {
                                 for (line of lines) {
                                     if (line.startsWith('Try this: ')) {
-                                        client.error('has try this' + line)
                                         line = line.replace(/Try this: /, '')
                                         line = line.replace(/\([0-9]+ ms\)/, '')
-                                        client.error('final line' + line)
                                         const action: CodeAction = {
                                             title: `Replace ${method} with ${line}`,
                                             kind: CodeActionKind.QuickFix,
@@ -85,15 +78,13 @@ export async function activate(context: ExtensionContext): Promise<void> {
                                                 }],
                                             },
                                         }
-                                        client.error(`action: ${action}`)
                                         resolve([action])
                                     }
                                 }
-                                return null
-                            }, () => null)
+                            })
                         }
                     }
-                }, () => null)
+                })
             })
         }
     }
@@ -107,6 +98,7 @@ export async function activate(context: ExtensionContext): Promise<void> {
 
     client.onReady().then(() => {
         client.onNotification("PIDE/dynamic_output", (params) => {
+            client.info('got dynamic output')
             isaBuffer.setLines(params.content.split('\n'), {start: 0, end: -1, strictIndexing: false})
             defaultHandler("PIDE/dynamic_output", params)
         })
@@ -124,17 +116,17 @@ export async function activate(context: ExtensionContext): Promise<void> {
             arglist: ['bufname("%")'],
             callback: sendCaretUpdate,
         })
-        workspace.nvim.command('vsplit').then(() => {
-            workspace.nvim.command('b isabelle-output').then(() => {
-                // TODO: use nvim.feedKeys if possible
-                workspace.nvim.input(`L`)
-            })
-        })
         workspace.document.then((doc) => doc.buffer.name.then((name) => sendCaretUpdate(name)))
     })
 
-
     context.subscriptions.push(
+        commands.registerCommand('isabelle.openOutput', () => {
+            workspace.nvim.command('vsplit').then(() => {
+                workspace.nvim.setBuffer(isaBuffer).then(() => {
+                    workspace.nvim.input('Lh')
+                })
+            })
+        }),
         services.registLanguageClient(client),
     )
 }
